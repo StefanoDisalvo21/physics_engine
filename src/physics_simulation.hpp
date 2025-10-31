96b54cbb772a0e23ec9b230fcea2bf1d69437903
StefanoDisalvo21 <stefanodisalvo21@gmail.com>
2025-10-31 18:39:17 +0100

    feat: add physics class declaration

--- src/physics_simulation.hpp
+++ src/physics_simulation.hpp
@@ -2,3 +2,14 @@
 #include "dimensions.hpp"
 #include <iostream>
 using namespace std;
+
+class Physics_Simualtion {
+    private:
+        Position body_coordinate;
+        float radius = 1.0f;
+    public:
+        const float get_x() const{ return body_coordinate.x;};
+        const float get_y() const{ return body_coordinate.y;};
+        const float get_z() const{ return body_coordinate.z;};
+        const float get_radius() const { return radius;}
+};
